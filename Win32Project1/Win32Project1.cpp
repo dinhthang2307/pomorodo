a3c79d7a78ffe7835a9353ac15d6748eb2420beb
dinhthang2307 <dinhthang2307@gmail.com>
2016-10-06 01:49:55 +0700

    Demo1St

--- Win32Project1/Win32Project1.cpp
+++ Win32Project1/Win32Project1.cpp
@@ -1,9 +1,13 @@
-// Win32Project1.cpp : Defines the entry point for the application.
+﻿// Win32Project1.cpp : Defines the entry point for the application.
 //
 
 #include "stdafx.h"
 #include "Win32Project1.h"
 #include "Commctrl.h"
+
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#pragma comment(lib, "ComCtl32.lib")
+#include <time.h>
 #define MAX_LOADSTRING 100
 
 // Global Variables:
@@ -16,6 +20,7 @@ ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    Alert(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -74,7 +79,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 	wcex.hInstance = hInstance;
 	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WIN32PROJECT1));
 	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.hbrBackground = (HBRUSH)(CTLCOLOR_BTN + 1);
 	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_WIN32PROJECT1);
 	wcex.lpszClassName = szWindowClass;
 	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
@@ -128,31 +133,56 @@ int iNumberOfPomodoro = 0;
 HWND ValueOfBreakTime;
 HWND ValueOfSessionTime;
 HWND ValueOfPomodoros;
+WCHAR* bufferTimeBreak = new WCHAR[255];
+WCHAR* bufferSessionTime = new WCHAR[255];
+WCHAR* bufferNumberOfPomodoro = new WCHAR[255];
+bool bSessionRunning = true;
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	
+
 	switch (message)
 	{
 
 	case WM_CREATE: {
-		
+
+		INITCOMMONCONTROLSEX icc;
+		icc.dwSize = sizeof(icc);
+		icc.dwICC = ICC_WIN95_CLASSES;
+		InitCommonControlsEx(&icc);
+
+		// Lấy font hệ thống
+		LOGFONT lf;
+		GetObject(GetStockObject(DEFAULT_GUI_FONT), sizeof(LOGFONT), &lf);
+		HFONT hFont = CreateFont(lf.lfHeight, lf.lfWidth,
+			lf.lfEscapement, lf.lfOrientation, lf.lfWeight,
+			lf.lfItalic, lf.lfUnderline, lf.lfStrikeOut, lf.lfCharSet,
+			lf.lfOutPrecision, lf.lfClipPrecision, lf.lfQuality,
+			lf.lfPitchAndFamily, lf.lfFaceName);
+
+
+
 		//draw bread area
 		HWND MinusButtonBreakTime = CreateWindowEx(0, L"BUTTON", L"-", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,  // Styles 
-			450,          
-			50,          
-			20,        
-			20,        
-			hWnd,     
+			450,
+			50,
+			20,
+			20,
+			hWnd,
 			(HMENU)IDC_BUTTON1,
 			(HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE),
-			NULL);      
+			NULL);
+		SendMessage(MinusButtonBreakTime, WM_SETFONT, WPARAM(hFont), TRUE);
 
-		HWND AddButtonBreakTime = CreateWindow(L"BUTTON", L"+", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-			600, 50,20, 20, hWnd, (HMENU)IDC_BUTTON2,      
+		HWND AddButtonBreakTime = CreateWindowEx(0,L"BUTTON", L"+", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+			600, 50, 20, 20, hWnd, (HMENU)IDC_BUTTON2,
 			(HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE),
-			NULL);      
+			NULL);
+
+		SendMessage(AddButtonBreakTime, WM_SETFONT, WPARAM(hFont), TRUE);
+
 		//static text
-		HWND TimeBreak = CreateWindow(
+		HWND TimeBreak = CreateWindowEx(0,
 			L"Static",  // Predefined class; Unicode assumed 
 			L"Break Time",      // Button text 
 			WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
@@ -164,11 +194,12 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			NULL,       // No menu.
 			(HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE),
 			NULL);      // Pointer not needed.
+		SendMessage(TimeBreak, WM_SETFONT, WPARAM(hFont), TRUE);
 
-		ValueOfBreakTime = CreateWindow(
+		ValueOfBreakTime = CreateWindowEx(0,
 			L"Edit",  // Predefined class; Unicode assumed 
 			L"5",      // Button text 
-			ES_LEFT | WS_BORDER | WS_TABSTOP | WS_VISIBLE | WS_CHILD,  // Styles 
+			ES_LEFT | WS_BORDER | WS_TABSTOP | WS_VISIBLE | WS_CHILD|ES_READONLY,  // Styles 
 			475,         // x position 
 			50,         // y position 
 			100,        // Button width
@@ -177,9 +208,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			NULL,       // No menu.
 			(HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE),
 			NULL);      // Pointer not needed.
+		SendMessage(ValueOfBreakTime, WM_SETFONT, WPARAM(hFont), TRUE);
 		//*********************************************************************************************//
 		//draw Session area
-		HWND MinusButtonSessionTime = CreateWindow(
+		HWND MinusButtonSessionTime = CreateWindowEx(0,
 			L"BUTTON",  // Predefined class; Unicode assumed 
 			L"-",      // Button text 
 			WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
@@ -191,8 +223,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			(HMENU)IDC_BUTTON3,       // No menu.
 			(HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE),
 			NULL);      // Pointer not needed.
-
-		HWND AddButtonSession = CreateWindow(
+		SendMessage(MinusButtonBreakTime, WM_SETFONT, WPARAM(hFont), TRUE);
+		HWND AddButtonSession = CreateWindowEx(0,
 			L"BUTTON",  // Predefined class; Unicode assumed 
 			L"+",      // Button text 
 			WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
@@ -204,8 +236,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			(HMENU)IDC_BUTTON4,       // No menu.
 			(HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE),
 			NULL);      // Pointer not needed.
+		SendMessage(AddButtonSession, WM_SETFONT, WPARAM(hFont), TRUE);
 						//static text
-		HWND TimeSession = CreateWindow(
+		HWND TimeSession = CreateWindowEx(0,
 			L"Static",  // Predefined class; Unicode assumed 
 			L"Session Time",      // Button text 
 			WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
@@ -217,11 +250,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			NULL,       // No menu.
 			(HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE),
 			NULL);      // Pointer not needed.
-
-		ValueOfSessionTime = CreateWindow(
+		SendMessage(TimeSession, WM_SETFONT, WPARAM(hFont), TRUE);
+		ValueOfSessionTime = CreateWindowEx(0,
 			L"Edit",  // Predefined class; Unicode assumed 
 			L"25",      // Button text 
-			ES_LEFT | WS_BORDER | WS_TABSTOP | WS_VISIBLE | WS_CHILD,  // Styles 
+			ES_LEFT | WS_BORDER | WS_TABSTOP | WS_VISIBLE | WS_CHILD|ES_READONLY,  // Styles 
 			475,         // x position 
 			200,         // y position 
 			100,        // Button width
@@ -230,8 +263,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			NULL,       // No menu.
 			(HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE),
 			NULL);      // Pointer not needed.
-
-		HWND NumberOfPomdoros = CreateWindow(
+		SendMessage(ValueOfSessionTime, WM_SETFONT, WPARAM(hFont), TRUE);
+		HWND NumberOfPomdoros = CreateWindowEx(0,
 			L"Static",  // Predefined class; Unicode assumed 
 			L"Number Of Pomdoros:",      // Button text 
 			ES_LEFT | WS_BORDER | WS_TABSTOP | WS_VISIBLE | WS_CHILD,  // Styles 
@@ -243,11 +276,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			NULL,       // No menu.
 			(HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE),
 			NULL);      // Pointer not needed.
-
-		ValueOfPomodoros = CreateWindow(
+		SendMessage(NumberOfPomdoros, WM_SETFONT, WPARAM(hFont), TRUE);
+		ValueOfPomodoros = CreateWindowEx(0,
 			L"Edit",  // Predefined class; Unicode assumed 
-			L"0",     
-			ES_LEFT | WS_BORDER | WS_TABSTOP | WS_VISIBLE | WS_CHILD,  // Styles 
+			L"0",
+			ES_CENTER | WS_BORDER | WS_TABSTOP | WS_VISIBLE | WS_CHILD|ES_READONLY,  // Styles 
 			455,         // x position 
 			300,         // y position 
 			200,        // Button width
@@ -256,8 +289,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			NULL,       // No menu.
 			(HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE),
 			NULL);      // Pointer not needed.
-
-		HWND StartButton = CreateWindow(
+		SendMessage(ValueOfPomodoros, WM_SETFONT, WPARAM(hFont), TRUE);
+		HWND StartButton = CreateWindowEx(0,
 			L"BUTTON",  // Predefined class; Unicode assumed 
 			L"Start",      // Button text 
 			WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
@@ -270,8 +303,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			(HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE),
 			NULL);      // Pointer not needed.
 						//static text
-
-		HWND ResetButton = CreateWindow(
+		SendMessage(StartButton, WM_SETFONT, WPARAM(hFont), TRUE);
+		HWND ResetButton = CreateWindowEx(0,
 			L"BUTTON",  // Predefined class; Unicode assumed 
 			L"Reset",      // Button text 
 			WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
@@ -282,9 +315,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			hWnd,     // Parent window
 			(HMENU)IDC_BUTTON6,       // No menu.
 			(HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE),
-			NULL);      // Pointer not needed.
-						//static text
-
+			NULL);      
+		SendMessage(ResetButton, WM_SETFONT, WPARAM(hFont), TRUE);
 		return 0;
 	}break;
 
@@ -296,20 +328,98 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		{
 		case IDC_BUTTON1: {
 			iTimeBreak--;
+			wsprintf(bufferTimeBreak, L"%d", iTimeBreak);
+			SetWindowText(ValueOfBreakTime, bufferTimeBreak);
+
+			if (!bufferTimeBreak)
+				delete[] bufferTimeBreak;
 		}break;
 		case IDC_BUTTON2: {
 			iTimeBreak++;
+			wsprintf(bufferTimeBreak, L"%d", iTimeBreak);
+			SetWindowText(ValueOfBreakTime, bufferTimeBreak);
+
+			if (!bufferTimeBreak)
+				delete[] bufferTimeBreak;
 		}break;
 		case IDC_BUTTON3: {
 			iSessionTime--;
+			wsprintf(bufferSessionTime, L"%d", iSessionTime);
+			SetWindowText(ValueOfSessionTime, bufferSessionTime);
+
+			if (!bufferSessionTime)
+				delete[] bufferSessionTime;
 		}break;
 		case IDC_BUTTON4: {
 			iSessionTime++;
+			wsprintf(bufferSessionTime, L"%d", iSessionTime);
+			SetWindowText(ValueOfSessionTime, bufferSessionTime);
+
+			if (!bufferSessionTime)
+				delete[] bufferSessionTime;
 		}break;
-		case IDC_BUTTON5: {
+		case IDC_BUTTON6: {
 			iTimeBreak = 5;
-			iSessionTime=25 ;
+			iSessionTime = 25;
+
+
+			wsprintf(bufferSessionTime, L"%d", iSessionTime);
+			wsprintf(bufferTimeBreak, L"%d", iTimeBreak);
+			SetWindowText(ValueOfSessionTime, bufferSessionTime);
+			SetWindowText(ValueOfBreakTime, bufferTimeBreak);
+
+
+			if (!bufferSessionTime)
+				delete[] bufferSessionTime;
+			if (!bufferTimeBreak)
+				delete[] bufferTimeBreak;
 		}break;
+
+		case IDC_BUTTON5: {
+
+			//main code here
+			int iSecondSession = 60 * iSessionTime;
+			int iSecondBreak = 60 * iTimeBreak;
+			int iSecondPassed;
+			int lStartTime = clock();
+			for(;;) {
+				if (bSessionRunning == true) {
+					iSecondPassed = (clock() - lStartTime);
+					if (iSecondPassed >= iSecondSession) {
+						DialogBox(hInst, MAKEINTRESOURCE(IDD_ALERTBOX), NULL, Alert);
+						iNumberOfPomodoro++;
+						bSessionRunning = false;
+						wsprintf(bufferNumberOfPomodoro, L"%d", iNumberOfPomodoro);
+						SetWindowText(ValueOfPomodoros, bufferNumberOfPomodoro);
+						lStartTime = clock();
+					}
+				}
+				else if(bSessionRunning==false){
+					if (iNumberOfPomodoro < 4) {
+						iSecondPassed = (clock() - lStartTime);
+						if (iSecondPassed >= iSecondBreak) {
+							DialogBox(hInst, MAKEINTRESOURCE(IDD_ALERTBOX), NULL, Alert);
+							bSessionRunning = true;
+							lStartTime = clock();
+						}
+					}
+					else {
+						iSecondPassed = (clock() - lStartTime);
+						if (iSecondPassed >= 15*60) {
+							DialogBox(hInst, MAKEINTRESOURCE(IDD_ALERTBOX), NULL, Alert);
+							bSessionRunning = true;
+							lStartTime = clock();
+							iNumberOfPomodoro = 0;
+						}
+					}
+				}
+				if (!bufferSessionTime)
+					delete[] bufferSessionTime;
+
+			}
+			
+		}break;
+
 		case IDM_ABOUT:
 			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
 			break;
@@ -355,5 +465,25 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 		}
 		break;
 	}
+	return (INT_PTR)FALSE;
+}
+
+INT_PTR CALLBACK Alert(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	
 	return (INT_PTR)FALSE;
 }
